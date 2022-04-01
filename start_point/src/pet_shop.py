8d6e38492c0091a722f56a81dfaad47c9b27861c
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-04-01 13:04:03 +0100

    test 17

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -60,3 +60,7 @@ def remove_customer_cash(customer_buy,how_much):
 
 def get_customer_pet_count(customer):
     return len(customer["pets"])
+
+
+def add_pet_to_customer(customer,new_pet):
+    customer["pets"].append(new_pet)
